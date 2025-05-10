7659f5cc6d227c2367bc54ad968535449aee5624
PhosFactum <phosphorus.work@gmail.com>
2025-05-10 20:04:51 +0300

    fix(air): no bins; fix(orders): empty file

--- internal/models/orders.go
+++ internal/models/orders.go
@@ -1,15 +1,5 @@
 package models
 
-
-type User struct {
-	ID       uint   `json:"id" gorm:"primaryKey"`
-	Name     string `json:"name"`
-	Email    string `json:"email" gorm:"unique"`
-	Age      int    `json:"age"`
-	Password string `json:"password"`
-}
-
-
 type Order struct {
 	ID        uint    `json:"id" gorm:"primaryKey"`
 	UserID    uint    `json:"user_id"`
---
+++ internal/models/users.go
@@ -0,0 +1,10 @@
+package models
+
+
+type User struct {
+	ID       uint   `json:"id" gorm:"primaryKey"`
+	Name     string `json:"name"`
+	Email    string `json:"email" gorm:"unique"`
+	Age      int    `json:"age"`
+	Password string `json:"password"`
+}
